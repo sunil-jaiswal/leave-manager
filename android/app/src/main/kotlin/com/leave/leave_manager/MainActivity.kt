69caf6d469496403f51d595017096ebc69f65bdf
Sunil Jaiswal <suniljaiswal002@gmail.com>
2021-03-22 18:39:02 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/leave/leave_manager/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.leave.leave_manager
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/config/theme_config.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class ThemeConfig{
+   final ThemeData themeData = ThemeData(
+
+  );
+}
